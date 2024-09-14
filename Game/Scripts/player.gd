78c27fb1ae87101a2567f11fe23bf1d788a42daa
Tom Douaud <tom.douaud@iut-rodez.fr>
2024-09-14 11:01:41 -0400

    ADD player death and power ups

--- Game/Scripts/player.gd
+++ Game/Scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
+const POWER_UP_TIMER = 3
 
 var is_facing_right = true
 var wait_for_anim_end = false
@@ -11,11 +11,16 @@ var is_crouching = false # ras le cul, j'aimerais apprendre les anim tree mais t
 var is_running = false # si quelqu'un vois ce code je n'ai plus aucune crédibilitée dans l'univers
 var is_punching = false
 
+var player_speed = 100.0
 var player_life = 5;
 var player_damage = 1;
 
 var direction
-
+func _ready():
+	$AnimatedSprite2D_Power.visible = false
+	$AnimatedSprite2D_Speed.visible = false
+	pickup_power_up(2)
+	
 func _physics_process(delta: float) -> void:
 	direction = Input.get_axis("ui_left", "ui_right")
 	is_facing_right = false if direction == -1 else true
@@ -36,13 +41,13 @@ func _physics_process(delta: float) -> void:
 	else:
 		# The Movement
 		if direction and (!is_crouching and !is_punching): # Crouch as the priority over movement
-			velocity.x = direction * SPEED
+			velocity.x = direction * player_speed
 			play_anim("run", !is_facing_right)
 			is_running = true
 		else:
 			# If the player does nothing, idle
 			if !is_crouching and !is_punching:
-				velocity.x = move_toward(velocity.x, 0, SPEED)
+				velocity.x = move_toward(velocity.x, 0, player_speed)
 				play_anim("idle", !is_facing_right)
 				is_punching = false
 				is_crouching = false
@@ -86,11 +91,50 @@ func change_collision(collision1 : bool, collision2: bool):
 	$CollisionShape2D.disabled = !collision1
 	$CollisionShape2D_Crouch.disabled = !collision2
 
-func play_anim(anim_name : String, face_direction : bool, play_forward: bool = true):
+func play_anim(anim_name : String, face_direction : bool = true, play_forward: bool = true):
 	$AnimatedSprite2D_Main.animation = anim_name
+	$AnimatedSprite2D_Power.animation = anim_name + "_red"
+	$AnimatedSprite2D_Speed.animation = anim_name + "_yellow"
 	$AnimatedSprite2D_Main.flip_h = face_direction
+	$AnimatedSprite2D_Power.flip_h = face_direction
+	$AnimatedSprite2D_Speed.flip_h = face_direction
+	$AnimatedSprite2D_Main.centered = true
+
+	
 	if play_forward:
 		$AnimatedSprite2D_Main.play()
+		$AnimatedSprite2D_Power.play()
+		$AnimatedSprite2D_Speed.play()
 	else:
 		$AnimatedSprite2D_Main.play_backwards()
+		$AnimatedSprite2D_Power.play_backwards()
+		$AnimatedSprite2D_Speed.play_backwards()
 		
+func take_damage(damage: int):
+	player_life -= damage
+	if player_life < 1:
+		play_anim("die")
+		# TODO do something after the death
+	else:
+		play_anim("hurt")
+		
+func pickup_power_up(type):
+	$Timer.wait_time = POWER_UP_TIMER
+	$Timer.one_shot = true
+	# Power power-up
+	if type == 1:
+		player_damage += 1
+		$AnimatedSprite2D_Power.visible = true
+		$Timer.start()
+	elif type == 2:
+		player_speed = player_speed * 2
+		$AnimatedSprite2D_Speed.visible = true
+		$Timer.start()
+	else:
+		player_life += 1
+	
+func _on_timer_timeout() -> void:
+	$AnimatedSprite2D_Power.visible = false
+	$AnimatedSprite2D_Speed.visible = false
+	player_speed = 100.0
+	player_damage = 1
