7d618f7682f7329ab3e3f282f0dfd2682107dd53
Aguix <aguix.contact+git@gmail.com>
2024-09-15 03:23:42 -0400

    [ADD] InGameUI v2.0

--- Game/Scenes/Components/UI/in_game_ui.gd
+++ Game/Scenes/Components/UI/in_game_ui.gd
@@ -1,16 +1,25 @@
 extends CanvasLayer
-
-func update_health(new_value : int):
-	$MarginContainer/HBoxContainer/PlayerHealth/HBoxContainer/PlayerHealthLabel.text = new_value
 	
-func show_power_up(type):
+func show_power_up(type, value = null):
 	match type :
-		1: $MarginContainer/HBoxContainer/PlayerPowerUps/Damage.show()
-		2: $MarginContainer/HBoxContainer/PlayerPowerUps/Speed.show()
-		3: $"MarginContainer/HBoxContainer/PlayerPowerUps/Life".show()
+		1: $MarginContainer/HBoxContainer/PlayerPowerUps/Damage.show(); if value: update_damage(value)
+		2: $MarginContainer/HBoxContainer/PlayerPowerUps/Speed.show(); if value: update_speed(value)
+		3: $"MarginContainer/HBoxContainer/PlayerPowerUps/Life".show(); if value: update_life(value)
 
 func hide_power_up(type):
 	match type :
 		1: $MarginContainer/HBoxContainer/PlayerPowerUps/Damage.hide()
 		2: $MarginContainer/HBoxContainer/PlayerPowerUps/Speed.hide()
 		3: $"MarginContainer/HBoxContainer/PlayerPowerUps/Life".hide()
+
+func update_health(value : int):
+	$MarginContainer/HBoxContainer/PlayerHealth/HBoxContainer/PlayerHealthLabel.text = str(value)
+
+func update_damage(value : int):
+	$MarginContainer/HBoxContainer/PlayerPowerUps/Damage/HBoxContainer/DamageLabel.text = str(value)
+
+func update_speed(value : int):
+	$MarginContainer/HBoxContainer/PlayerPowerUps/Speed/HBoxContainer/SpeedLabel.text = str(value)
+	
+func update_life(value : int):
+	$MarginContainer/HBoxContainer/PlayerPowerUps/Life/HBoxContainer/LifeLabel.text = str(value);
