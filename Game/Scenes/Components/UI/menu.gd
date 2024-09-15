c7f4af8b8b90e468c4388a61f8bad09b5264f1d5
Tom Douaud <tom.douaud@iut-rodez.fr>
2024-09-15 10:37:19 -0400

    ADD Sounds for UI

--- Game/Scenes/Components/UI/menu.gd
+++ Game/Scenes/Components/UI/menu.gd
@@ -1,9 +1,16 @@
 extends Control
 
+var confirm_sound = preload("res://Assets/Sounds/UI/UI_confirm.wav")
 
 func _on_play_pressed() -> void:
+	$AudioStreamPlayer2D.stream = confirm_sound
+	$AudioStreamPlayer2D.play()
+	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
 func _on_quit_game_pressed() -> void:
+	$AudioStreamPlayer2D.stream = confirm_sound
+	$AudioStreamPlayer2D.play()
+	await get_tree().create_timer(1.0).timeout
 	get_tree().quit()
