bcd00f3db01218b9885a6e1386e61dce5289e2ab
Tom Douaud <tom.douaud@iut-rodez.fr>
2024-09-15 12:02:45 -0400

    Add pause music and quit button

--- Game/Scenes/Components/UI/PauseMenu/pause_menu.gd
+++ Game/Scenes/Components/UI/PauseMenu/pause_menu.gd
@@ -2,5 +2,21 @@ extends Control
 
 signal resume
 
+func _ready() -> void:
+	$PauseMusic.play()
+	
 func _on_button_pressed() -> void:
+	$ConfirmButton.play()
+	await get_tree().create_timer(1.0).timeout
+	$PauseMusic.stop()
 	resume.emit()
+
+func _on_button_2_pressed() -> void:
+	$ConfirmButton.play()
+	await get_tree().create_timer(1.0).timeout
+	$PauseMusic.stop()
+	get_tree().quit()
+	
+func _physics_process(delta: float) -> void:
+	if !$"PauseMusic".is_playing():
+			$"PauseMusic".play()
